a34a3dc2b3cf9ef901e15c9dc0a3412ab6bdd24f
stkr953 <96133763+stkr953@users.noreply.github.com>
2021-12-29 22:43:44 +0530

    Add files via upload

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+
+
+// Returns jwt token if registration was successful
+// * @public
+// */
+exports.register = async (req, res, next) => {
+
+
+};
+
+/**
+ * Returns jwt token if valid username and password is provided
+ * @public
+ */
+exports.login = async (req, res, next) => {
+}
