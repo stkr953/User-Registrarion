ff44944504d3f2475ec3a94b4136eb7029452442
stkr953 <96133763+stkr953@users.noreply.github.com>
2021-12-30 08:37:11 +0530

    Add files via upload

---
+++ src/validations/auth.validations.js
@@ -0,0 +1,19 @@
+module.exports = {
+    // POST /v1/auth/register
+}
+ // POST /v1/auth/login
+ login: {
+}
+
+// POST /v1/auth/facebook
+// POST /v1/auth/google
+oAuth: {
+}
+
+// POST /v1/auth/refresh
+sendPasswordReset: {
+}
+
+// POST /v1/auth/password-reset
+passwordReset: {
+}
---
+++ src/validations/user,validation.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+
+    // GET /v1/users
+}
+// POST /v1/users
+createUser :{
+
+}
+
+// PUT /v1/users/:userId
+replaceUser: {
+}
+
+// PATCH /v1/users/:userId
+updateUser: {
+}
